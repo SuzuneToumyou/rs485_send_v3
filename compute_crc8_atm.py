0f872bf3af5f95a9eded5aa0dab9013a09f7d032
ぬこむーちょ <30884264+SuzuneToumyou@users.noreply.github.com>
2023-08-24 16:05:53 +0900

    Add files via upload

---
+++ compute_crc8_atm.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*
+
+def compute_crc8_atm(datagram, initial_value=0):
+    crc = initial_value
+    # Iterate bytes in data
+    for byte in datagram:
+        # Iterate bits in byte
+        for _ in range(0, 8):
+            if (crc >> 7) ^ (byte & 0x01):
+                crc = ((crc << 1) ^ 0x07) & 0xFF
+            else:
+                crc = (crc << 1) & 0xFF
+            # Shift to next bit
+            byte = byte >> 1
+    return crc
